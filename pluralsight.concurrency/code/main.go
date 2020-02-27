cbc47e920ae4a9af84c820bc020615923afe1220
Eli <e_corrales2003@yahoo.com>
2020-02-27 08:52:22 -0500

    .

--- pluralsight.concurrency/code/main.go
+++ pluralsight.concurrency/code/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 )
 
-const maxBooks = 50
+var maxBooks = 200
+var sleepTime time.Duration = 80
+var doConcurrently = false
 
 var cache = map[int]Book{}
 
@@ -16,7 +20,29 @@ var lenOfCache = 0
 
 func main() {
 
+	configureAccordingToParams()
+
 	queryCacheAndDatabaseLoop()
+}
+
+func configureAccordingToParams() {
+
+	loops, mbErr := strconv.Atoi(os.Args[1])
+	sleep, slErr := strconv.Atoi(os.Args[2])
+	if len(os.Args) > 3 && os.Args[3] == "m" {
+		doConcurrently = true
+	}
+	println(loops)
+	fmt.Printf("loops err: %s\n", mbErr)
+	println(sleep)
+	fmt.Printf("loop sleep err: %s\n", slErr)
+
+	if mbErr != nil || slErr != nil {
+		os.Exit(1)
+	}
+
+	maxBooks = loops
+	sleepTime = time.Duration(sleep)
 
 }
 
@@ -26,59 +52,65 @@ func queryCacheAndDatabaseLoop() {
 
 	for i := 0; i < maxBooks; i++ {
 
-		if lenOfCache == 10 {
+		print(lenOfCache)
+		print(" ")
+
+		if lenOfCache == len(books) {
 			println("All books in cache")
 			break
 		}
 
-		id := rnd.Intn(10) + 1
+		id := rnd.Intn(len(books)) + 1
 		//id := i + 1
 
-		queryCacheAndDatabase(id)
+		if doConcurrently {
+			go queryCache(id)
+			go queryDatabase(id)
+		} else {
+			queryCache(id)
+			queryDatabase(id)
+		}
 
-		//gives cache time to receive new data in concurrent operation (???)
-		time.Sleep(120 * time.Millisecond)
+		// if we dont have this, then all threads(goroutines)
+		// are quickly started, then this loop is done,
+		// then main quits before all the goroutines
+		// have a chance to run
+		time.Sleep(sleepTime * time.Millisecond)
+	}
+
+	println("eli")
+	println(lenOfCache)
+	println(len(books))
+
+	for lenOfCache < len(books) {
+		println("Waiting on cache to fill")
+		time.Sleep(1000 * time.Millisecond)
 	}
 
 	end := time.Now().UnixNano()
 
 	delta := end - start
 
+	println("")
 	println("------------------------")
 	println(delta / 1000000)
 
 }
 
-func queryCacheAndDatabase(id int) {
-
-	go func(id int) {
-		if _, ok := queryCache(id); ok {
-			fmt.Printf("In cache : %d, %t, %+v\n", len(cache))
-			lenOfCache = len(cache)
-		}
-	}(id)
-
-	go queryDatabase(id)
-	//queryDatabase(id)
-
+func queryCache(id int) {
+	if _, ok := cache[id]; ok {
+		lenOfCache = len(cache)
+	}
 }
 
-func queryCache(id int) (Book, bool) {
+func queryDatabase(id int) {
 
-	b, ok := cache[id]
-	return b, ok
-
-}
-
-func queryDatabase(id int) (Book, bool) {
-
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(400 * time.Millisecond)
 	for _, b := range books {
 		if b.ID == id {
 			//add found book to cache
 			cache[id] = b
-			return b, true
+			return
 		}
 	}
-	return Book{}, false
 }
