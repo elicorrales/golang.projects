5a42bd273895bcc482042e51cde9178a772024b9
Eli <e_corrales2003@yahoo.com>
2020-03-21 19:00:18 -0400

    .

--- usb.master/main.go
+++ usb.master/main.go
@@ -8,12 +8,13 @@ import (
 func main() {
 
     files, err := ioutil.ReadDir("/dev")
+    //files, err := ioutil.ReadDir("c:/Users/devchu")
     if err != nil {
         println(err)
         os.Exit(1)
     }
 
     for _, file := range files {
-        println(file)
+        println(file.Name())
     }
 }
